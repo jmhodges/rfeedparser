7a758f105448a1e405ae621de4298f75abc68361
Jeff Hodges <jmh@jeff-hodges-computer.local>
2007-07-21 21:24:16 -0700

    indentation in better_attributelist

--- lib/rfeedparser/better_attributelist.rb
+++ lib/rfeedparser/better_attributelist.rb
@@ -8,31 +8,31 @@ module XML
   module SAX
     module AttributeList # in xml/sax.rb
       def [](key)
-	getValue(key)
+        getValue(key)
       end
 
       def each(&blk)
-	(0...getLength).each{|pos| yield [getName(pos), getValue(pos)]}
+        (0...getLength).each{|pos| yield [getName(pos), getValue(pos)]}
       end
 
       def each_key(&blk)
-	(0...getLength).each{|pos| yield getName(pos) }
+        (0...getLength).each{|pos| yield getName(pos) }
       end
 
       def each_value(&blk)
-	(0...getLength).each{|pos| yield getValue(pos) }
+        (0...getLength).each{|pos| yield getValue(pos) }
       end
 
       def to_a # Rather use collect? grep for to_a.collect
-	l = []
-	each{|k,v| l << [k,v]}
-	return l
+        l = []
+        each{|k,v| l << [k,v]}
+        return l
       end
 
       def to_s
-	l = []
-	each{|k,v| l << "#{k} => #{v}"}
-	"{ "+l.join(", ")+" }"
+        l = []
+        each{|k,v| l << "#{k} => #{v}"}
+        "{ "+l.join(", ")+" }"
       end
     end
   end
