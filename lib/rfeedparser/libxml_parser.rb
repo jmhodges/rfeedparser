a5c23b21031cffb67bc96f9b34d960d405c6aaa1
Phil Hagelberg <technomancy@gmail.com>
2008-05-06 17:18:33 -0700

    cleaning up libxml parser

--- lib/rfeedparser/libxml_parser.rb
+++ lib/rfeedparser/libxml_parser.rb
@@ -23,54 +23,25 @@ module FeedParser
 
      attr_accessor :bozo, :entries, :feeddata, :exc
      def initialize(baseuri, baselang, encoding)
-       $stderr << "trying StrictFeedParser\n" if $debug
+       $stderr.puts "trying LibXml::StrictFeedParser" if $debug
        startup(baseuri, baselang, encoding)
        @bozo = false
-       @exc = nil
-       super()
      end
-
-     #def getPos
-       #[@locator.getSystemId, @locator.getLineNumber]
-     #end
-
-     def getAttrs(attrs)
-       ret = []
-       for i in 0..attrs.getLength
-   ret.push([attrs.getName(i), attrs.getValue(i)])
-       end
-       ret
-     end
-
-     #def setDocumentLocator(loc)
-       #@locator = loc
-     #end
-
-     #def startDoctypeDecl(name, pub_sys, long_name, uri)
-       ##Nothing is done here. What could we do that is neat and useful?
-     #end
-
-     #def startNamespaceDecl(prefix, uri)
-       #trackNamespace(prefix, uri)
-     #end
-
-     #def endNamespaceDecl(prefix)
-     #end
 
      def on_start_element(name, attrs)
        name =~ /^(([^;]*);)?(.+)$/ # Snag namespaceuri from name
-   namespaceuri = ($2 || '').downcase
+       namespaceuri = ($2 || '').downcase
        name = $3
        if /backend\.userland\.com\/rss/ =~ namespaceuri
-   # match any backend.userland.com namespace
-   namespaceuri = 'http://backend.userland.com/rss'
+         # match any backend.userland.com namespace
+         namespaceuri = 'http://backend.userland.com/rss'
        end
        prefix = @matchnamespaces[namespaceuri]
-       # No need to raise UndeclaredNamespace, Expat does that for us with
-       "unbound prefix (XMLParserError)"
+
        if prefix and not prefix.empty?
-   name = prefix + ':' + name
+         name = prefix + ':' + name
        end
+
        name.downcase!
        unknown_starttag(name, attrs)
      end
@@ -85,34 +56,18 @@ module FeedParser
 
      def on_end_element(name)
        name =~ /^(([^;]*);)?(.+)$/ # Snag namespaceuri from name
-   namespaceuri = ($2 || '').downcase
+       namespaceuri = ($2 || '').downcase
        prefix = @matchnamespaces[namespaceuri]
        if prefix and not prefix.empty?
-   localname = prefix + ':' + name
+         localname = prefix + ':' + name
        end
        name.downcase!
        unknown_endtag(name)
      end
 
-     def on_comment(comment)
-       handle_comment(comment)
-     end
-
-     #def entityDecl(*foo)
-     #end
-
-     #def unparsedEntityDecl(*foo)
-     #end
-
      def on_parser_error(exc)
        @bozo = true
        @exc = exc
-       raise exc
-     end
-
-     def on_parser_fatal_error(exc)
-       # Lib xml doesn't seem to call this??
-       on_parser_error(exc)
        raise exc
      end
    end
